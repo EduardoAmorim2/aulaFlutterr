fd4a18c94c25b3c9e1b4d8f84f9b24c9b358633e
EduardoAmorim2 <eduardoamorim855@gmail.com>
2023-05-02 21:19:49 -0300

    Add plugin screenutil;

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 void main() {
   runApp(const MyApp());
@@ -54,170 +55,176 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Row(
-                      children: [
-                        GestureDetector(
-                          onTap: () {
-                            isClicked = isClicked ? false : true;
-                            setState(() {});
-                          },
-                          child: smallContainer(
-                            color: isClicked ? containerColors[0] : Colors.red,
-                            size: _sizeMultiplied ? 400 : 200,
-                            child: Column(
-                              children: [
-                                Row(
-                                  children: [
-                                    GestureDetector(
-                                      onTap: () {
-                                        isClicked = isClicked ? false : true;
-                                        setState(() {});
-                                      },
-                                      child: smallContainer(
-                                        size: _sizeMultiplied ? 100 : 50,
-                                        color: isClicked
-                                            ? containerColors[1]
-                                            : Colors.green,
-                                        child: Text('1'),
-                                      ),
-                                    ),
-                                    GestureDetector(
-                                      onTap: () {
-                                        isClicked = isClicked ? false : true;
-                                        setState(() {});
-                                      },
-                                      child: smallContainer(
-                                        size: _sizeMultiplied ? 100 : 50,
-                                        color: isClicked
-                                            ? containerColors[2]
-                                            : Colors.blue,
-                                        child: Text('2'),
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                                Row(
-                                  children: [
-                                    GestureDetector(
-                                      onTap: () {
-                                        isClicked ? false : true;
-                                        setState(() {});
-                                      },
-                                      child: smallContainer(
-                                        size: _sizeMultiplied ? 100 : 50,
-                                        color: isClicked
-                                            ? containerColors[3]
-                                            : Colors.black,
-                                        child: Text(
-                                          '3',
-                                          style: TextStyle(color: Colors.white),
+    return ScreenUtilInit(builder: (context, child) {
+      return Scaffold(
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+        body: Center(
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Row(
+                        children: [
+                          GestureDetector(
+                            onTap: () {
+                              isClicked = isClicked ? false : true;
+                              setState(() {});
+                            },
+                            child: smallContainer(
+                              color:
+                                  isClicked ? containerColors[0] : Colors.red,
+                              size: _sizeMultiplied ? 50.w : 25.w,
+                              child: Column(
+                                children: [
+                                  Row(
+                                    children: [
+                                      GestureDetector(
+                                        onTap: () {
+                                          isClicked = isClicked ? false : true;
+                                          setState(() {});
+                                        },
+                                        child: smallContainer(
+                                          size: _sizeMultiplied ? 10.w : 5.w,
+                                          color: isClicked
+                                              ? containerColors[1]
+                                              : Colors.green,
+                                          child: Text('1'),
                                         ),
                                       ),
-                                    ),
-                                    GestureDetector(
-                                      onTap: () {
-                                        isClicked ? false : true;
-                                        setState(() {});
-                                      },
-                                      child: smallContainer(
-                                        size: _sizeMultiplied ? 100 : 50,
-                                        color: isClicked
-                                            ? containerColors[4]
-                                            : Colors.white,
-                                        child: Text('4'),
+                                      GestureDetector(
+                                        onTap: () {
+                                          isClicked = isClicked ? false : true;
+                                          setState(() {});
+                                        },
+                                        child: smallContainer(
+                                          size: _sizeMultiplied ? 10.w : 5.w,
+                                          color: isClicked
+                                              ? containerColors[2]
+                                              : Colors.blue,
+                                          child: Text('2'),
+                                        ),
                                       ),
-                                    ),
-                                  ],
-                                ),
-                                Spacer(),
-                                Row(
-                                  crossAxisAlignment: CrossAxisAlignment.end,
-                                  mainAxisAlignment: MainAxisAlignment.end,
-                                  children: [
-                                    Text('5'),
-                                  ],
-                                ),
-                              ],
+                                    ],
+                                  ),
+                                  Row(
+                                    children: [
+                                      GestureDetector(
+                                        onTap: () {
+                                          isClicked ? false : true;
+                                          setState(() {});
+                                        },
+                                        child: smallContainer(
+                                          size: _sizeMultiplied ? 10.w : 5.w,
+                                          color: isClicked
+                                              ? containerColors[3]
+                                              : Colors.black,
+                                          child: Text(
+                                            '3',
+                                            style:
+                                                TextStyle(color: Colors.white),
+                                          ),
+                                        ),
+                                      ),
+                                      GestureDetector(
+                                        onTap: () {
+                                          isClicked ? false : true;
+                                          setState(() {});
+                                        },
+                                        child: smallContainer(
+                                          size: _sizeMultiplied ? 10.w : 5.w,
+                                          color: isClicked
+                                              ? containerColors[4]
+                                              : Colors.white,
+                                          child: Text('4'),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                  Spacer(),
+                                  Row(
+                                    crossAxisAlignment: CrossAxisAlignment.end,
+                                    mainAxisAlignment: MainAxisAlignment.end,
+                                    children: [
+                                      Text('5'),
+                                    ],
+                                  ),
+                                ],
+                              ),
                             ),
                           ),
-                        ),
-                        GestureDetector(
-                          onTap: () {
-                            isClicked = isClicked ? false : true;
-                            setState(() {});
-                          },
-                          child: smallContainer(
-                            size: _sizeMultiplied ? 400 : 200,
-                            color:
-                                isClicked ? containerColors[5] : Colors.green,
-                            child: Text('6'),
+                          GestureDetector(
+                            onTap: () {
+                              isClicked = isClicked ? false : true;
+                              setState(() {});
+                            },
+                            child: smallContainer(
+                              size: _sizeMultiplied ? 50.w : 25.w,
+                              color:
+                                  isClicked ? containerColors[5] : Colors.green,
+                              child: Text('6'),
+                            ),
                           ),
-                        ),
-                      ],
-                    ),
-                    Row(
-                      children: [
-                        GestureDetector(
-                          onTap: () {
-                            isClicked = isClicked ? false : true;
-                            setState(() {});
-                          },
-                          child: smallContainer(
-                            size: _sizeMultiplied ? 400 : 200,
-                            color: isClicked ? containerColors[6] : Colors.blue,
-                            child: Text('7'),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          GestureDetector(
+                            onTap: () {
+                              isClicked = isClicked ? false : true;
+                              setState(() {});
+                            },
+                            child: smallContainer(
+                              size: _sizeMultiplied ? 50.w : 25.w,
+                              color:
+                                  isClicked ? containerColors[6] : Colors.blue,
+                              child: Text('7'),
+                            ),
                           ),
-                        ),
-                        GestureDetector(
-                          onTap: () {
-                            isClicked = isClicked ? false : true;
-                            setState(() {});
-                          },
-                          child: smallContainer(
-                            size: _sizeMultiplied ? 400 : 200,
-                            color:
-                                isClicked ? containerColors[7] : Colors.yellow,
-                            child: Text('8'),
+                          GestureDetector(
+                            onTap: () {
+                              isClicked = isClicked ? false : true;
+                              setState(() {});
+                            },
+                            child: smallContainer(
+                              size: _sizeMultiplied ? 50.w : 25.w,
+                              color: isClicked
+                                  ? containerColors[7]
+                                  : Colors.yellow,
+                              child: Text('8'),
+                            ),
                           ),
-                        ),
-                      ],
-                    )
-                  ],
-                ),
-              ],
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            ElevatedButton(
-              onPressed: () {
-                radius = radius == 0 ? 200 : 0;
-                setState(() {});
-              },
-              child: Text('Teste'),
-            )
-          ],
+                        ],
+                      )
+                    ],
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  radius = radius == 0 ? 25.w : 0;
+                  setState(() {});
+                },
+                child: Text('Teste'),
+              )
+            ],
+          ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+        floatingActionButton: FloatingActionButton(
+          onPressed: _incrementCounter,
+          tooltip: 'Increment',
+          child: const Icon(Icons.add),
+        ), // This trailing comma makes auto-formatting nicer for build methods.
+      );
+    });
   }
 
   Container smallContainer(
